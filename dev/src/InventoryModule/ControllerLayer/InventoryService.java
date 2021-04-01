ff9f05d704e331c5bd0952da45c7012e51928c9e
Edan Levy <edal@post.bgu.ac.il>
2021-04-01 16:25:45 +0300

    InventoryService AddItem method

--- dev/src/InventoryModule/ControllerLayer/InventoryService.java
+++ dev/src/InventoryModule/ControllerLayer/InventoryService.java
@@ -1,4 +1,16 @@
 package InventoryModule.ControllerLayer;
 
+import InventoryModule.BusinessLayer.Category;
+
+import java.util.List;
+
 public interface InventoryService {
+
+    public Response addItem(int id, String name, Category category,
+                            double costPrice, double sellingPrice, int minAmount,
+                            String shelfLocation, String storageLocation,
+                            int storageQuantity, int shelfQuantity,
+                            int manufacturerId, List<Integer> suppliersId, int supplyTime);
 }
+
+
