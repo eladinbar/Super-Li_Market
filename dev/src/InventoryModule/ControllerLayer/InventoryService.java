ac2e9f9c31eac0afdf61dff24883adbf46ee3fb6
eladinbar <eladin@post.bgu.ac.il>
2021-04-02 13:41:38 +0300

    Removed redundant 'public' privacy modifiers in InventoryService interface

--- dev/src/InventoryModule/ControllerLayer/InventoryService.java
+++ dev/src/InventoryModule/ControllerLayer/InventoryService.java
@@ -9,66 +9,63 @@ import java.util.List;
 
 public interface InventoryService {
 
-    public Response addItem(int id, String name, String category,
+    Response addItem(int id, String name, String category,
                             double costPrice, double sellingPrice, int minAmount,
                             String shelfLocation, String storageLocation,
                             int storageQuantity, int shelfQuantity,
                             int manufacturerId, List<Integer> suppliersId);
-    public ResponseT<Item> getItem(int ItemId);
-    public Response modifyItemName(int itemId, String newName);
-    public Response modifyItemCategory(int itemId, String newCategoryName);
-    public Response modifyItemCostPrice(int itemId, double newCostPrice);
-    public Response modifyItemSellPrice(int itemId, double newSellName);
+    ResponseT<Item> getItem(int ItemId);
+    Response modifyItemName(int itemId, String newName);
+    Response modifyItemCategory(int itemId, String newCategoryName);
+    Response modifyItemCostPrice(int itemId, double newCostPrice);
+    Response modifyItemSellPrice(int itemId, double newSellName);
     /*
     if the locations are null, the location will stay the same.
      */
-    public Response changeItemLocation(int itemId, String newStorageLocation, String newStoreLocation);
+    Response changeItemLocation(int itemId, String newStorageLocation, String newStoreLocation);
     /*
     If quantity is a negative number the quantity of the Respectable argument will not be changed.
      */
-    public Response modifyItemQuantity(int itemId, int newStorageQuantity, int newStoreQuantity);
-    public Response addItemSupplier(int itemId, int supplierId);
-    public Response removeItemSupplier(int itemId, int supplierId);
+    Response modifyItemQuantity(int itemId, int newStorageQuantity, int newStoreQuantity);
+    Response addItemSupplier(int itemId, int supplierId);
+    Response removeItemSupplier(int itemId, int supplierId);
 
     //-------------------------------------------------------------------------
 
     /*
     If parent category is null, the new category should be added as a main category.
      */
-    public Response addCategory(String categoryName, String parentCategory);
-    public ResponseT<Category> getCategory(String categoryName);
-    public Response modifyCategoryName(String OldName, String newName);
+    Response addCategory(String categoryName, String parentCategory);
+    ResponseT<Category> getCategory(String categoryName);
+    Response modifyCategoryName(String OldName, String newName);
     /*
     when the category is deleted all its sub category move to the parent category.
      */
-    public Response removeCategory(String categoryName);
+    Response removeCategory(String categoryName);
 
     //-------------------------------------------------------------------------
 
-    public Response addItemSale(String saleName,int itemID, double saleDiscount, Date startDate, Date endDate);
-    public Response addCategorySale(String saleName,String categoryName, double saleDiscount, Date startDate, Date endDate);
-    public Response modifySaleName(String oldName, String newName);
-    public Response modifySaleDiscount(String saleName, double newDiscount);
-    public Response modifySaleDates(String saleName, Date startDate, Date endDate);
+    Response addItemSale(String saleName,int itemID, double saleDiscount, Date startDate, Date endDate);
+    Response addCategorySale(String saleName,String categoryName, double saleDiscount, Date startDate, Date endDate);
+    Response modifySaleName(String oldName, String newName);
+    Response modifySaleDiscount(String saleName, double newDiscount);
+    Response modifySaleDates(String saleName, Date startDate, Date endDate);
 
     //-------------------------------------------------------------------------
 
-    public Response addItemDiscount(String Supplier, double discount, Date discountDate, int itemCount, int itemId);
-    public Response addCategoryDiscount(String Supplier, double discount, Date discountDate, int itemCount, String categoryName);
+    Response addItemDiscount(String Supplier, double discount, Date discountDate, int itemCount, int itemId);
+    Response addCategoryDiscount(String Supplier, double discount, Date discountDate, int itemCount, String categoryName);
 
     //-------------------------------------------------------------------------
 
-    public Response recordDefect(int itemId, String itemName, int defectQuantity, String defectLocation);
+    Response recordDefect(int itemId, String itemName, int defectQuantity, String defectLocation);
 
     //-------------------------------------------------------------------------
 
-    public ResponseT<List<Item>> inventoryReport();
-    public ResponseT<List<Item>> itemShortageReport();
-    public ResponseT<List<DefectEntry>> defectsReport(Date fromDate, Date toDate);
-    public ResponseT<List<Item>> categoryReport(Date fromDate, Date toDate);
-
-
-
+    ResponseT<List<Item>> inventoryReport();
+    ResponseT<List<Item>> itemShortageReport();
+    ResponseT<List<DefectEntry>> defectsReport(Date fromDate, Date toDate);
+    ResponseT<List<Item>> categoryReport(Date fromDate, Date toDate);
 }
 
 
