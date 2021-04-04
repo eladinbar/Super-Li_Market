b6348914e511fd49322c6a3853785be9be0e7528
ofirpovi <ofirpovi@bgu.ac.il>
2021-04-04 11:15:57 +0300

    Employee in Business layer

--- dev/src/Employees/business_layer/Employee/Role.java
+++ dev/src/Employees/business_layer/Employee/Role.java
@@ -1,4 +1,11 @@
 package Employees.business_layer.Employee;
 
 public enum Role {
+    branchManager,
+    branchManagerAssistent,
+    humanResourcesMananger,
+    cashier,
+    guard,
+    usher,
+    storeKeeper
 }
