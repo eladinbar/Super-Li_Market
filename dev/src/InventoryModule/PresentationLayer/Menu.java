bf383c0c595bc2da0071d5e40affbb00a9cd3393
edal <edal@post.bgu.ac.il>
2021-04-07 19:40:40 +0300

    marge resolve in menu

--- dev/src/InventoryModule/PresentationLayer/Menu.java
+++ dev/src/InventoryModule/PresentationLayer/Menu.java
@@ -2,7 +2,6 @@ package InventoryModule.PresentationLayer;
 
 import InfrastructurePackage.TextFormatter;
 import InventoryModule.ControllerLayer.SimpleObjects.*;
-
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
