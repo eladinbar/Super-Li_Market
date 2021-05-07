c97927cc65ef9f312f91baf3e1a7c5aee93a0c7d
dollshak <dollshak@post.bgu.ac.il>
2021-05-07 14:31:03 +0300

    added fields and constructor to supplier dal objects

--- dev/src/DataAccessLayer/DalObjects/SupplierObjects/SupplierCard.java
+++ dev/src/DataAccessLayer/DalObjects/SupplierObjects/SupplierCard.java
@@ -1,10 +1,30 @@
 package DataAccessLayer.DalObjects.SupplierObjects;
 
+import BusinessLayer.SupliersPackage.supplierPackage.Payment;
 import DataAccessLayer.DalControllers.DalController;
 import DataAccessLayer.DalObjects.DalObject;
 
+import java.util.List;
+
 public class SupplierCard extends DalObject<SupplierCard> {
+    private int supplierId; //foreign key of personCard
+    private int companyNumber;
+    private boolean isPernamentDays;
+    private boolean selfDelivery;
+    private Payment payment;
+    private List<String> contactMembers;
+
     protected SupplierCard(DalController<SupplierCard> controller) {
         super(controller);
+    }
+
+    public SupplierCard(DalController<SupplierCard> controller, int supplierId, int companyNumber, boolean isPernamentDays, boolean selfDelivery, Payment payment, List<String> contactMembers) {
+        super(controller);
+        this.supplierId = supplierId;
+        this.companyNumber = companyNumber;
+        this.isPernamentDays = isPernamentDays;
+        this.selfDelivery = selfDelivery;
+        this.payment = payment;
+        this.contactMembers = contactMembers;
     }
 }
